3ed7a432d09839000dd07bc9b48e86043af575d2
git-soundhar <p.soundhar10@gmail.com>
2024-07-31 22:00:18 +0530

    increment and decrement load the updated data to car logo is completed

---
+++ js/allProducts.js
@@ -0,0 +1,77 @@
+let stockView=document.getElementById('stockView');
+let generateStoreRoom = () => {
+   return  stockView.innerHTML = stocks.map((x) => {
+        let { id, productName, veiety, quantity, rate, productImg } = x;
+        return `
+            <div class="productCard" id="product-id-${id}">
+               <div class="cardImgContainer">
+                  <img src="${productImg}"/>
+               </div>
+               <div class="productDescription">
+                 <p class="productVariety">${veiety}</p> <!-- Corrected 'veiety' to 'variety' -->
+                 <h4 class="productName">${productName}</h4>
+                 <p class="quantity">${quantity} grams</p>
+               </div>
+               <div class="purchaseArea">
+                  <span class="productRate">&#8377; ${rate}</span>
+                  <div class="decideQuantity">
+                    <i onclick="decrement('${id}')" class="fa-solid fa-minus"></i>
+                    <div id="${id}" class="quantities">0</div>
+                    <i onclick="increment('${id}')" class="fa-solid fa-plus"></i>
+                  </div>
+               </div>
+           </div>
+        `;
+    }).join("");
+};
+
+generateStoreRoom();
+
+let addedStocks=[];
+
+//usedto increase the quantity
+let increment=(id)=>{
+    let selectedItem=id;
+    let search=addedStocks.find(x=>x.id===selectedItem);
+    console.log(search);
+    if(search==undefined){
+        addedStocks.push({id:`${id}`,item:1})
+        
+        console.log(addedStocks);
+    }
+    else{
+        search.item +=1;
+        console.log(addedStocks);
+    }
+    update(selectedItem);
+};
+// used to decrease the quantity
+let decrement=(id)=>{
+    let selectedItem=id;
+    let search=addedStocks.find(x=>x.id===selectedItem);
+    if(search===undefined){
+        alert(`you never added ${id}`);
+    }
+    else if(search.item===0) return;
+    else{
+        search.item -=1;
+        console.log(search);
+    }
+    // used to show increment of the quantity
+    update(selectedItem);
+}; 
+
+
+// used to update the quantity
+let update=(id)=>{
+   let search=addedStocks.find(x=>x.id===id);
+   document.getElementById(id).innerHTML=search.item;
+   console.log(addedStocks,"console from update");
+   mainCart();
+}
+
+let mainCart=()=>{
+   let totalProduct=addedStocks.map(x=>x.item);
+   let mainCartNotification=document.getElementById('badge');
+   mainCartNotification.innerHTML=totalProduct.reduce((x,y)=>x+y,0);
+}
---
+++ js/data.js
@@ -0,0 +1,399 @@
+
+let stocks=[
+    {
+        id:"dindigul briyani mix",
+        productName:"DINDIGUL BRIYANI MIX 250 GM",
+        veiety:"Briyani mix and Combo",
+        quantity:250,
+        rate:185,
+        productImg:"../nandrimasalaimages/dindigulbriyanimasala.png"
+    },
+    {
+        id:"dindigul briyani mix and rice",
+        productName:"DINDIGUL BRIYANI MIX + RICE",
+        veiety:"Briyani mix and Combo",
+        quantity:500,
+        rate:249,
+        productImg:"../nandrimasalaimages/dindigulbriyani&rice.png"
+    },
+    {
+        id:"cicken 65 masala",
+        productName:"RESTAURANT STYLE CHICKEN 65 MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:108,
+        productImg:"../nandrimasalaimages/chicken65.png"
+    },
+    {
+        id:"mutton masala",
+        productName:"MUTTON MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:149,
+        productImg:"../nandrimasalaimages/muttonmasal.png"
+    },
+    {
+        id:"madurai mutton chukka",
+        productName:"MADURAI MUTTON CHUKKA MASALA",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:209,
+        productImg:"../nandrimasalaimages/maduraimuttonchukka.png"
+    },
+    {
+        id:"varuthacurryi masala",
+        productName:"VILLAGE VARUTHA CURRY MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:149,
+        productImg:"../nandrimasalaimages/varuthakari.png"
+    },
+    {
+        id:"pepper chicken",
+        productName:"CHETTINAD PEPPER CHICKEN MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:195,
+        productImg:"../nandrimasalaimages/pepperchicken.png"
+    },
+    {
+        id:"fish roast masala",
+        productName:"FISH ROAST MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:132,
+        productImg:"../nandrimasalaimages/fishroast.png"
+    },
+    {
+        id:"meen kuzhambu masala",
+        productName:"MEENAVAR MEEN KUZHAMBU MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:113,
+        productImg:"../nandrimasalaimages/meenavarmeenkuzhambu.png"
+    },
+    {
+        id:"grab masala",
+        productName:"NANDU MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:218,
+        productImg:"../nandrimasalaimages/nandumasala.png"
+    },
+    {
+        id:"curry masala",
+        productName:"CURRY MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:133,
+        productImg:"../nandrimasalaimages/currymasala.png"
+    },
+    {
+        id:"garam masala",
+        productName:"GARAM MASAL 200 GM",
+        price:185,
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:233,
+        productImg:"../nandrimasalaimages/garammasala.png"
+    },
+    {
+        id:"egg curry masala",
+        productName:"EGG CURRY MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:223,
+        productImg:"../nandrimasalaimages/eggcurrymasala.png"
+    },
+    {
+        id:"sambar powder",
+        productName:"SAMBAR PODI 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:123,
+        productImg:"../nandrimasalaimages/sambarpodi.png"
+    },
+    {
+        id:"kuzhambu masala",
+        productName:"KUZHAMBU MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:135,
+        productImg:"../nandrimasalaimages/kuzhambumasala.png"
+    },
+    {
+        id:"idly powder",
+        productName:"IDLY PODI 200 GM",
+        veiety:"Masala andSpices",
+        quantity:200,
+        rate:137,
+        productImg:"../nandrimasalaimages/idlypodi.png"
+    },
+    {
+        id:"pulisadham powder",
+        productName:"PULI SADHAM PODI 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:124,
+        productImg:"../nandrimasalaimages/pulisadhampodi.png"
+    },
+    {
+        id:"dindigul briyani combo",
+        productName:"DINDIGUL BRIYANI COMBO",
+        veiety:"Briyani mix and Combo",
+        quantity:1100,
+        rate:760.5,
+        productImg:"../nandrimasalaimages/dindigulbriyanicombo.png"
+    },
+    {
+        id:"special combo",
+        productName:"SPECIAL COMBO",
+        veiety:"Briyani mix and Combo",
+        quantity:800,
+        rate:674.1,
+        productImg:"../nandrimasalaimages/specialcombo.png"
+    },
+    {
+        id:"chicken masala ",
+        productName:"CHICKEN MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:152,
+        productImg:"../nandrimasalaimages/chickenmasala.png"
+    },
+    {
+        id:"madurai kari kuszhambu masala",
+        productName:"MADURAI CURRY KUZHAMBU MASALA 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:149,
+        productImg:"../nandrimasalaimages/maduraikarikuzhambumasala.png"
+    },
+    {
+        id:"rasam podi",
+        productName:"RASAM PODI 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:191,
+        productImg:"../nandrimasalaimages/rasampodi.png"
+    },
+    {
+        id:"andhra paruppu sadha podi",
+        productName:"ANDHRA PARUPPU SADHJA PODI 250 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:106,
+        productImg:"../nandrimasalaimages/andhraparuppusadham.png"
+    },
+    {
+        id:"kadamba pickle",
+        productName:"KASI MEDU KADAMBA PICKLE",
+        veiety:"pickle and sambal",
+        quantity:250,
+        rate:330,
+        productImg:"../nandrimasalaimages/kasimedukadambapickle.png"
+    },
+    {
+        id:"prawn sambal",
+        productName:"TUTICORI PRAWN SAMBAL",
+        price:185,
+        veiety:"pickle and sambal",
+        quantity:250,
+        rate:260,
+        productImg:"../nandrimasalaimages/prawnsambal.png"
+    },
+    {
+        id:"srilankan masi sambal",
+        productName:"SRILANKAN MASI SAMBAL",
+        veiety:"Pickel and Sambal",
+        quantity:120,
+        rate:220,
+        productImg:"../nandrimasalaimages/srilankanmaasisambal.png"
+    },
+    {
+        id:"rameswaram prawn pickle",
+        productName:"RAMESHWARAM PRAWN PICKLE",
+        veiety:"Pickle and Sambal",
+        quantity:250,
+        rate:350,
+        productImg:"../nandrimasalaimages/rameswarameralpickle.png"
+    },
+    {
+        id:"maldives maasi pickle",
+        productName:"MALDIVES MASI PICKLE",
+        veiety:"Pickle and Sambal",
+        quantity:250,
+        rate:330,
+        productImg:"../nandrimasalaimages/maldivesmasipickle.png"
+    },
+    {
+        id:"vanjiram dry fish pickle",
+        productName:"VANJIRAM DRY FISH PICKLE",
+        veiety:"Briyani mix and Combo",
+        quantity:250,
+        rate:430,
+        productImg:"../nandrimasalaimages/vanjaramdryfishpickle.png"
+    },
+    {
+        id:"vanjiram fish pickle",
+        productName:"VANJIRAM FISH PICKLE",
+        veiety:"Pickle and Sambal",
+        quantity:250,
+        rate:450,
+        productImg:"../nandrimasalaimages/vanjaramfishpickle.png"
+    },
+    {
+        id:"virudhunagar mutton pickle",
+        productName:"VIRUDHUNAGAR MUTTON PICKLE",
+        veiety:"Pickle and Sambal",
+        quantity:250,
+        rate:450,
+        productImg:"../nandrimasalaimages/virudhunagarmuttonpickle.png"
+    },
+    {
+        id:"chettinad chicken pickle",
+        productName:"CHETTINAD CHICKEN PICKLE",
+        veiety:"Pickle and Sambal",
+        quantity:250,
+        rate:330,
+        productImg:"../nandrimasalaimages/chettinadchickenpickle.png"
+    },
+    {
+        id:"groundnut oil",
+        productName:"GROUNDNUT OIL 2L",
+        veiety:"Oil and Ghee",
+        quantity:2000,
+        rate:640,
+        productImg:"../nandrimasalaimages/groundnutoil.png"
+    },
+    {
+        id:"ghee",
+        productName:"GHEE 1 L",
+        veiety:"Oil and Ghee",
+        quantity:1000,
+        rate:958,
+        productImg:"../nandrimasalaimages/ghee.jpg"
+    },
+    {
+        id:"chettinad briyani mix",
+        productName:"CHETTINAADU DUM BRIYANI MASALA 200 GM",
+        veiety:"Briyani mix and Combo",
+        quantity:200,
+        rate:210,
+        productImg:"../nandrimasalaimages/chettinaddum.png"
+    },
+    {
+        id:"kollupodi",
+        productName:"KOLLU IDLY PODI 200 GM",
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:105,
+        productImg:"../nandrimasalaimages/kollupodi.png"
+    },
+    {
+        id:"ghee500",
+        productName:"GHEE 500 ML",
+        veiety:"Oil and Ghee",
+        quantity:500,
+        rate:488,
+        productImg:"../nandrimasalaimages/ghee.jpg"
+    },
+    {
+        id:"ghee200",
+        productName:"GHEE 200 ML",
+        veiety:"Oil and Ghee",
+        quantity:200,
+        rate:202,
+        productImg:"../nandrimasalaimages/ghee.jpg"
+    },
+    {
+        id:"coconut oil",
+        productName:"COCONUT OIL 1L",
+        veiety:"Oil and Ghee",
+        quantity:1000,
+        rate:315,
+        productImg:"../nandrimasalaimages/coconut\ oil.jpg"
+    },
+    {
+        id:"groundnut oil 1",
+        productName:"GROUNDNUT OIL 1L",
+        veiety:"Oil and Ghee",
+        quantity:1000,
+        rate:325,
+        productImg:"../nandrimasalaimages/groundnutoil.png"
+    },
+    {
+        id:"groundnut oil 500",
+        productName:"GROUNDNUT OIL 500 ML",
+        veiety:"Oil and Ghee",
+        quantity:500,
+        rate:167,
+        productImg:"../nandrimasalaimages/groundnutoil.png"
+    },
+    {
+        id:"seasame 1l",
+        productName:"SEASAME OIL 1000 ML",
+        veiety:"Oil and Ghee",
+        quantity:1000,
+        rate:550,
+        productImg:"../nandrimasalaimages/seasameoil1l.jpg"
+    },
+    {
+        id:"seasame 500",
+        productName:"SEASAME OIL 500 ML",
+        veiety:"Oil and Ghee",
+        quantity:500,
+        rate:280,
+        productImg:"../nandrimasalaimages/seasameoil500.png"
+
+    },
+    {
+        id:"dindigul briyani mix 1",
+        productName:"DINDIGUL BRIYANI MIX 1 KG",
+        veiety:"Briyani mix and Combo",
+        quantity:1000,
+        rate:462,
+        productImg:"../nandrimasalaimages/briyanimixsingle.png"
+    },
+    {
+        id:"kollu rasam",
+        productName:"KONGUNAATU KOLLU RASAM PODI 200 GM",
+        price:141,
+        veiety:"Masala and Spices",
+        quantity:200,
+        rate:141,
+        productImg:"../nandrimasalaimages/kongu\ kollu\ rasam.png"
+    },
+    {
+        id:"coconut 500",
+        productName:"COCONUT OIL 500 ML",
+        veriety:"Oil and Ghee",
+        quantity:500,
+        rate:160,
+        productImg:"../nandrimasalaimages/coconut\ oil.jpg"
+    },
+    {
+        id:"salem naatu kozhi",
+        productName:"SALEM NAATU KOZHI MASALA",
+        veriety:"Masala and Spices",
+        quantity:200,
+        rate:180,
+        productImg:"../nandrimasalaimages/salemnaatukozhi.png"
+    },
+    {
+        id:"andhra paruppu sadha podi 500",
+        productName:"ANDHRA PARUPPU SADHJA PODI 5000 GM",
+        veiety:"Masala and Spices",
+        quantity:500,
+        rate:199,
+        productImg:'../nandrimasalaimages/idlypodi.png'
+    },
+    {
+        id:"idly podi 500",
+        productName:"IDLY PODI 500 GM",
+        veriety:"Masala and Spices",
+        quantity:500,
+        rate:297,
+        productImg:"../nandrimasalaimages/idlypodi.png"
+    }
+];
