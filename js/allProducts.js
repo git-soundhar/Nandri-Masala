7f0b67ba93c17064b34ccf5283fd3fdbf10c9849
git-soundhar <p.soundhar10@gmail.com>
2024-08-18 22:55:33 +0530

    increment decrement update maincart update work is completed

--- js/allProducts.js
+++ js/allProducts.js
@@ -1,9 +1,13 @@
-let stockView=document.getElementById('stockView');
+
+    let stockView=document.getElementById('stockView');
+    // this is the empty array to push and pop products
+let addedStocks = JSON.parse(localStorage.getItem('data')) || [];
 let generateStoreRoom = () => {
-   return  stockView.innerHTML = stocks.map((x) => {
-        let { id, productName, veiety, quantity, rate, productImg } = x;
+    return stockView.innerHTML = stocks.map((x) => {
+        let {id,productName,veiety, quantity,rate,productImg} = x;
+        let search = addedStocks.find((x) => x.id === id) || []; //i need more undersatand
         return `
-            <div class="productCard" id="product-id-${id}">
+               <div class="productCard" id="product-id-${id}" title="${productName}">
                <div class="cardImgContainer">
                   <img src="${productImg}"  alt="${productName}"/>
                </div>
@@ -16,63 +20,66 @@ let generateStoreRoom = () => {
                   <span class="productRate">&#8377; ${rate}</span>
                   <div class="decideQuantity">
                     <i onclick="decrement('${id}')" class="fa-solid fa-minus"></i>
-                    <div id="${id}" class="quantities">0</div>
+                    <div id="${id}" class="quantities">${ search.item === undefined ? 0 : search.item}</div>
                     <i onclick="increment('${id}')" class="fa-solid fa-plus"></i>
-                  </div>
+                   </div> 
                </div>
            </div>
-        `;
-    }).join("");
+        `
+}).join("");
 };
-
 generateStoreRoom();
 
-// this is the empty array to push and pop products
-let addedStocks=[];
-//function to increase the quantity of the product
+// function to increase the quantity of the product
+// let incDecNote=document.getElementById('incDecNote');
+
+
 let increment=(id)=>{
     let selectedItem=id;
-    let search=addedStocks.find(x=>x.id===selectedItem);
-    if(search==undefined){
-        addedStocks.push({id:selectedItem,item:1});
-        // console.log(addedStocks);
+    let search=addedStocks.find((x) => x.id === selectedItem);
+
+    if(search === undefined){
+        addedStocks.push({id:selectedItem,
+                          item:1
+                        });
     }
     else{
-        search.item+=1;
-        // console.log(addedStocks);
+        search.item += 1;
     }
-    update(id);
+   
+    update(id); 
+    cartUpdate();
+    localStorage.setItem('data',JSON.stringify(addedStocks));
 }
 
 // function to decrement the quantity of the product
 
 let decrement=(id)=>{
     let selectedItem=id;
-    let search=addedStocks.find(x=>x.id===selectedItem);
+    let search=addedStocks.find((x)=>x.id==selectedItem);
     if(search==undefined){
-        alert(`you never added ${id}`);
-        // console.log(addedStocks);
+        alert(`you  never added ${selectedItem}`);
     }
-    else if(search.item==0) return;
+    else if(search.item===0) return;
     else{
         search.item-=1;
-        // console.log(addedStocks);
     }
     update(id);
-}
-
-// function to update increased and decreased quantity of a product
-
-let update=(id)=>{
-    let search=addedStocks.find(x=>x.id===id);
-    console.log(search);
-    document.getElementById(id).innerHTML=search.item;
     cartUpdate();
+    addedStocks=addedStocks.filter((x) => x.item!=0);
+    localStorage.setItem('data',JSON.stringify(addedStocks));
 }
-// function to update the total quantity to the main cart logo
-let cartUpdate=()=>{
-    let total=addedStocks.map(x=>x.item);
+// function to update increased and decreased quantity of a product
+let update=(id)=>{
+   let updateqty=document.getElementById(id);
+   let search=addedStocks.find((x)=>x.id===id);
+   updateqty.innerHTML=search.item;
+}
 
-    document.getElementById('badge').innerHTML=total.reduce((x,y)=>x+y,0);
-    // console.log(current);
+// function to update the total quantity to the main cart logo
+
+let cartUpdate=()=>{
+  let totalQty=addedStocks.map((x)=>x.item);
+  document.getElementById('badge').innerHTML=totalQty.reduce((x,y)=>x+y,0);
 }
+cartUpdate();
--- js/index.js
+++ js/index.js
@@ -9,10 +9,21 @@ closeMenu.addEventListener('click',(e)=>{
     mobNav.classList.toggle("showMenu");
 });
 
-// onresize=()=>{
-// if(window.screen.width<=768){
-//     let homeBanner=document.getElementById('homebanner');
-//     homeBanner.attributes.src="./nandrimasalaimages/screensize.webp";
-//     alert("hello");
-// }
-// }
+
+let openCartIcon=document.getElementById('cartLogo');
+openCartIcon.addEventListener('click',(e)=>{
+    document.getElementById('mainCart').classList.toggle('cartOpen');
+    document.getElementById('mainNav').classList.toggle('opacityBackground');
+    document.getElementById('stockView').classList.toggle('opacityBackground');     
+});
+
+//code to close the cart
+let closeCartIcon=document.getElementById('closeCart');
+closeCartIcon.style='cursor:pointer';
+closeCartIcon.addEventListener('click',(e)=>{
+    document.getElementById('mainCart').classList.toggle('cartOpen');
+    // when i click the cart button the cart is open whil the background of body or other should be blured
+    document.getElementById('mainNav').classList.toggle('opacityBackground');
+    document.getElementById('mainCart').classList.add('smooth');
+    document.getElementById('stockView').classList.toggle('opacityBackground'); 
+});
